7403d6cdd4a8874361aae74f2ff4b281cd0a472e
Nicole Estefanía Trujillo Cañadas <netrujillo@espe.edu.ec>
2023-05-16 14:32:13 -0600

    Actividad

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    $(".contenedor-formularios").find("input, textarea").on("keyup blur focus", function (e) {
+
+        var $this = $(this),
+          label = $this.prev("label");
+
+        if (e.type === "keyup") {
+            if ($this.val() === "") {
+                label.removeClass("active highlight");
+            } else {
+                label.addClass("active highlight");
+            }
+        } else if (e.type === "blur") {
+            if($this.val() === "") {
+                label.removeClass("active highlight");
+                } else {
+                label.removeClass("highlight");
+                }
+        } else if (e.type === "focus") {
+            if($this.val() === "") {
+                label.removeClass("highlight");
+            }
+            else if($this.val() !== "") {
+                label.addClass("highlight");
+            }
+        }
+
+    });
+
+    $(".tab a").on("click", function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+
+        target = $(this).attr("href");
+
+        $(".contenido-tab > div").not(target).hide();
+
+        $(target).fadeIn(600);
+
+    });
+
+});
+
+function toggleConfidentialForm() {
+    var checkBox = document.getElementById("terceroCheckbox");
+    var confidentialForm = document.getElementById("confidentialForm");
+
+    if (checkBox.checked) {
+        confidentialForm.style.display = "block";
+    } else {
+        confidentialForm.style.display = "none";
+        }
+    }
+
+function enableSubmitButton() {
+    var propietarioCheckbox = document.getElementById("propietarioCheckbox");
+    var submitButton = document.getElementById("submitButton");
+
+    if (propietarioCheckbox.checked) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+        }
+}
