f98c5df8329cb8a7090e2ef1ef87a92e8fb353a8
Nicole Estefanía Trujillo Cañadas <netrujillo@espe.edu.ec>
2023-05-16 22:34:22 -0600

    Actividad

--- js/main.js
+++ js/main.js
@@ -66,3 +66,55 @@ function enableSubmitButton() {
         submitButton.disabled = true;
         }
 }
+      
+var valor, cadena ="", contador=0, estado=true;
+const form = document.getElementById("FormContacto");
+      
+function validar(){
+    estado=true;
+    const expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+    valor = document.getElementById("correo").value;
+    if (!expresion.test(valor)){
+        estado = false;
+        document.getElementById("correo").value="";
+        cadena+="Correo ";
+    }
+        valor = document.getElementById("telefono").value;
+    if(!((valor.length == 10) && (!isNaN(valor)))){
+        estado = false;
+        document.getElementById("telefono").value="";
+    if(cadena == ""){
+        cadena+="Telefono ";
+    }else{
+        cadena+="y Telefono ";
+        }
+    }
+
+    if(!estado){
+        alert("Error validación - corrija "+cadena);
+        //return false;
+    }else{
+        estado = true;
+        contador++;
+        valor= document.getElementById("Enviar");
+        valor.type = "button";
+        valor.click();
+            
+    }
+}
+
+function delay(n){
+          return new Promise(function(resolve){
+              setTimeout(resolve,n*1000);
+          });
+      }
+      
+async function desactivar() {
+  if (estado == true && contador != 0) {
+    const valor = document.getElementById("Enviar");
+    valor.disabled = true;
+    valor.value = "Enviando datos ..........";
+    this.form.submit();
+    await delay(100000);
+  }
+}
